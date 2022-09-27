67ab645022ae7e7625e7677a716c02f8f9e9c2b5
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2022-09-27 16:59:04 -0400

    Styling changes to cl_init.lua

--- lua/entities/cod-tac-insert/cl_init.lua
+++ lua/entities/cod-tac-insert/cl_init.lua
@@ -1,42 +1,36 @@
 include( "shared.lua" )
 
+local beamColor = Color( 128, 199, 103, 255 )
+local textColor = Color( 255, 255, 255, 255 )
+local laserMat = Material( "cable/hydra" )
 ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
 
 function ENT:Draw()
-
 	self:DrawShadow( false )
-	
 	self.Entity:DrawModel()
-
 end
 
-local Laser = Material( "cable/hydra" )
- 
 function ENT:Draw()
- 
 	self:DrawModel() 
- 
-	local Vector1 = self:GetPos() + self:GetRight() * -4.5
-	local Vector2 = self:GetPos() + self:GetRight() * -4.5 + Vector(0,0,40)
- 
-	render.SetMaterial( Laser )
-	render.DrawBeam( Vector1, Vector2, 17, 1, 1, Color( 128, 199, 103, 255 ) ) 
-  
-end 
+	local vector1 = self:GetPos() + self:GetRight() * -4.5
+	local vector2 = self:GetPos() + self:GetRight() * -4.5 + Vector( 0, 0, 40 )
 
-surface.CreateFont( "Arialf", { font = "Arial", antialias = true, size = 35 } )
+	render.SetMaterial( laserMat )
+	render.DrawBeam( vector1, vector2, 17, 1, 1, beamColor ) 
+end
 
+surface.CreateFont( "arialf", { font = "Arial", antialias = true, size = 35 } )
 
-hook.Add("HUDPaint","TacInsertText",function()
+hook.Add( "HUDPaint", "TacInsertText", function()
+	local visibleEnt = LocalPlayer():GetEyeTrace().Entity
+	if not visibleEnt:IsValid() then return end
+	if not LocalPlayer():GetEyeTrace().Entity then return end
 
-		local visible_entity = LocalPlayer():GetEyeTrace().Entity
-		if visible_entity:IsValid() then		 
-			if LocalPlayer():GetEyeTrace().Entity then
-				local entityClass = visible_entity:GetClass()
-				local player_to_entity_distance = LocalPlayer():GetPos():Distance(visible_entity:GetPos())
-				if (entityClass  == 'cod-tac-insert') then
-					draw.DrawText(visible_entity:GetNWString("TacOwner").."'s Tactical Insertion", "Arialf", ScrW()/2, ScrH()/2+150, Color(255, 255, 255, 255),TEXT_ALIGN_CENTER)							
-				end
-			end
-		end
-end)
+	local entityClass = visibleEnt:GetClass()
+	local textX = ScrW() / 2
+	local textY = ScrH() / 2 + 150
+	local playerToEntDistance = LocalPlayer():GetPos():Distance( visibleEnt:GetPos() )
+	if entityClass  == "cod-tac-insert" then
+		draw.DrawText( visibleEnt:GetNWString( "TacOwner" ).."'s Tactical Insertion", "arialf", textX, textY, textColor, TEXT_ALIGN_CENTER )
+	end
+end )
